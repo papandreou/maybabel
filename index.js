609aeb1cd93ed3f6f91a9215f67fd53a0c29196d
Andreas Lind <andreas@one.com>
2017-08-28 00:50:15 +0200

    Copy in an un-es6ified version of the sync pkg-up and its dependencies, restoring node.js <4 support

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 var semver = require('semver');
-var pkgUp = require('pkg-up');
 var fs = require('fs');
 var pathModule = require('path');
+
+// Un-es6ified version of https://github.com/sindresorhus/locate-path/blob/master/index.js#L14
+function locatePathSync(iterable, opts) {
+    var cwd = opts && opts.cwd;
+    if (typeof cwd === 'undefined') {
+        cwd = process.cwd();
+    }
+    for (var i = 0 ; i < iterable.length ; i += 1) {
+        el = iterable[i];
+		if (fs.existsSync(pathModule.resolve(cwd, el))) {
+			return el;
+		}
+	}
+};
+
+// Un-es6ified version of https://github.com/sindresorhus/find-up/blob/master/index.js#L28
+function pkgUpSync(filename, opts) {
+    opts = opts || {};
+
+    var dir = pathModule.resolve(opts.cwd || '');
+    var root = pathModule.parse(dir).root;
+
+    var filenames = [].concat(filename);
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        var file = locatePathSync(filenames, {cwd: dir});
+
+        if (file) {
+            return pathModule.join(dir, file);
+        } else if (dir === root) {
+            return null;
+        }
+
+        dir = pathModule.dirname(dir);
+    }
+};
+
 
 var requiredNodeVersion = fs.readFileSync(pathModule.resolve(pkgUp.sync().replace(/[^/]+$/, ''), '.nvmrc'), 'utf-8').trim();
 
